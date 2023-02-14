570775202193f090c53943d6f8ad76fff15a797d
iribastrillo <iribastrillo@gmail.com>
2023-02-14 20:50:00 -0300

    ADD: implementa validaciones en el form login.

--- src/components/App/Login/LoginForm/LoginForm.jsx
+++ src/components/App/Login/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import {useDispatch} from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -6,13 +6,29 @@ import Form from 'react-bootstrap/Form';
 import './LoginForm.css';
 import { login } from '../../../../services/dwallet/login';
 import { setLoggedInUser } from '../../../../app/session';
+import { useState } from 'react';
+
+import {isEmpty} from '../../../../utils/utils';
 
 
 const LoginForm = () => {
+    const [forbidLogin, setForbidLogin] = useState (true)
     const username = useRef ('');
     const password = useRef ('');
 
     const dispatch = useDispatch ();
+
+    useEffect (() => {
+        runLoginValidations ();
+    }, []);
+
+    function runLoginValidations () {
+        if (!isEmpty(username.current.value) && !isEmpty (password.current.value)) {
+            setForbidLogin (false);
+        } else {
+            setForbidLogin (true);
+        }
+    }
 
     function handleSubmit (event) {
         event.preventDefault();
@@ -29,20 +45,20 @@ const LoginForm = () => {
         <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-12">
             <Form.Label htmlFor='username'>Usuario</Form.Label>
-            <Form.Control ref={username} name='username' type="text" placeholder="Ingresá tu usuario..." />
+            <Form.Control onChange={runLoginValidations} ref={username} name='username' type="text" placeholder="Ingresá tu usuario..." />
             <Form.Text className="text-danger">
             </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3">
             <Form.Label htmlFor='password'>Contraseña</Form.Label>
-            <Form.Control ref={password} name='password' type="password" placeholder="Ingresá tu contraseña" />
+            <Form.Control onChange={runLoginValidations} ref={password} name='password' type="password" placeholder="Ingresá tu contraseña" />
             <Form.Text className="text-danger">
             </Form.Text>
         </Form.Group>
 
         <div className='d-grid'>
-            <Button variant="primary" type="submit" size='lg'>
+            <Button variant="primary" type="submit" size='lg' disabled={forbidLogin}>
                 Ingresá
             </Button>
             <p>Quiero registrarme</p>
