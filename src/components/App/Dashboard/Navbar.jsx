a7d3358acda22e9fbb5064c08bec9b9fa76fa745
fakytop <prgonzalo94@gmail.com>
2023-02-17 20:54:46 -0300

    navegacion a Dashboard

--- src/components/App/Dashboard/Navbar.jsx
+++ src/components/App/Dashboard/Navbar.jsx
@@ -21,6 +21,7 @@ const Navigation = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
+                        <Link to='/dashboard'><Nav.Link href="#home">Dashboard</Nav.Link></Link>
                         <Link to='/transactions'><Nav.Link href="#home">Mis movimientos</Nav.Link></Link>
                         <Link to='/'><Nav.Link onClick={handleLogOut}>Salir</Nav.Link></Link>
                     </Nav>
